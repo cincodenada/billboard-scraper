2b469d0436d2cdbfb067a9ce7a805eb5b8f6e7c8
Joel Bradshaw <cincodenada@gmail.com>
2015-10-29 15:09:04 -0700

    Fix DTS template parsing

--- cheater.pl
+++ cheater.pl
@@ -14,9 +14,9 @@ sub debug {
 
 while(<>) {
 	$_ = decode_entities($_);
-	if(/{{Dts\|format=\w+\|(\d+)\|(\d+)\|(\d+)}}/) {
+	if(/\{\{Dts\|format\=\w+\|(\d+)\|(\d+)\|(\d+)\}\}/) {
 		($year, $month, $day) = ($1, $2, $3);
-		s/{{Dts.*?}}/$month-$day-$year/;
+		s/\{\{Dts.*?\}\}/$month-$day-$year/;
 	}
 	s/<ref.*?>.*?<\/ref>//g;
 	s/<small>(.*?)<\/small>/(\1)/g;
